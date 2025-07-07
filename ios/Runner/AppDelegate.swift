fd1fb7cbb24ebac27508872396ee1270d8851dc8
zyad negm <zyadosamanegm@gmail.com>
2025-07-07 14:55:35 +0300

    ios fix3

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
 import Flutter
 import UIKit
+import flutter_local_notifications
 import Firebase
-
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,9 +9,13 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
     FirebaseApp.configure()
 
     GeneratedPluginRegistrant.register(with: self)
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
